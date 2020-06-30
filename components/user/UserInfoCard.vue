40d5df66492b45695c9192587c305c850a4d9bdc
Rusiano <mekhtievrs0@gmail.com>
2020-06-30 15:21:15 +0300

    feat(users): add page and info card
    cc #1

---
+++ components/user/UserInfoCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-col sm="12" md="6" xl="4">
+    <v-card>
+      <v-card-text>
+        <div class="overline">
+          Пользователь
+        </div>
+      </v-card-text>
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <v-avatar color="primary">
+            <img :src="user.attributes.avatar" :alt="user.attributes.login" />
+          </v-avatar>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title> @{{ user.attributes.login }} </v-list-item-title>
+          <v-list-item-subtitle
+            v-if="!user.attributes.verified_at"
+            class="red--text text--darken-4"
+          >
+            <v-icon x-small color="red darken-4"
+              >mdi-close-circle-outline</v-icon
+            >
+            Не подтвержден
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-email</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ user.attributes.email }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>
+            mdi-calendar
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ user.attributes.created_at }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'UserInfoCard',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
