c84da439051739d2970bc953d5970823ff9cd96e
Rusiano <mekhtievrs0@gmail.com>
2020-06-30 19:28:36 +0300

    feat(counterparties): add info card
    cc #5

---
+++ pages/counterparties/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="counterparty">
+    <counterparty-info-card
+      :counterparty="counterparty"
+    ></counterparty-info-card>
+  </div>
+</template>
+
+<script>
+import CounterpartyInfoCard from '../../components/counterparties/CounterpartyInfoCard'
+export default {
+  name: 'Id',
+  components: { CounterpartyInfoCard },
+  fetch: ({ store, params }) => {
+    return Promise.all([
+      store.dispatch('counterparties/loadById', {
+        id: params.id,
+        options: {
+          // XDEBUG_SESSION_START: 'PHPSTORM'
+        },
+      }),
+    ])
+  },
+
+  computed: {
+    counterparty() {
+      return this.$store.getters['counterparties/byId']({
+        id: this.$route.params.id,
+      })
+    },
+  },
+}
+</script>
